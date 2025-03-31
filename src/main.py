692f6bb43af0806264ffaba70f68dc7cc9b0a5cb
Leo Liu <11664880+silver886@users.noreply.github.com>
2025-03-31 13:16:24 -0700

    implement replay

--- src/main.py
+++ src/main.py
@@ -14,20 +14,34 @@ GPIO.setmode(GPIO.BCM)
 
 keyboard = evdev.InputDevice('/dev/input/by-id/usb-Dell_Dell_Wired_Multimedia_Keyboard-event-kbd')
 
-def display(enigma):
+def display(enigma, replay, replay_read):
     display_peripheral = HT16K33Segment14(i2c, board=HT16K33Segment14.SPARKFUN_ALPHA)
     display_peripheral.set_brightness(9)
     display_peripheral.clear()
     display_peripheral.draw()
     display = "    "
     while True:
-        for char in enigma.recv():
-            display = display[1:] + char
-            display_peripheral.clear()
-            for i in range(4):
-                display_peripheral.set_character(display[i], i)
-            display_peripheral.draw()
-            sleep(0.01)
+        if enigma.poll(0.001):
+            for char in enigma.recv():
+                display = display + char
+                display_peripheral.clear()
+                if char == "|":
+                    display = "    "
+                for i in range(4):
+                    display_peripheral.set_character(display[i-4], i)
+                display_peripheral.draw()
+                if char == "|":
+                    display = "    "
+                sleep(0.01)
+        elif replay.poll(0.001):
+            replay_read.send(True)
+            replay.recv()
+            for i in range(len(display)-3):
+                display_peripheral.clear()
+                for j in range(4):
+                    display_peripheral.set_character(display[i+j], j)
+                display_peripheral.draw()
+                sleep(0.33)
 
 def led(enigma):
     gpio = [21, 20, 16, 12]
@@ -45,7 +59,7 @@ def led(enigma):
             elif char == "f" or (char == "b" and not datetime.now().microsecond // 100000 < 5):
                 GPIO.output(gpio[i], False)
 
-def recall(enigma, enigma_read):
+def replay(display, display_read):
     GPIO.setup(25, GPIO.IN)
     sent = False
     pre = False
@@ -53,10 +67,9 @@ def recall(enigma, enigma_read):
         now = GPIO.input(25)
         if pre and not now and not sent:
             sent = True
-            enigma.send(True)
-            print("reset - request")
-        if enigma_read.poll(0.001):
-            enigma_read.recv()
+            display.send(True)
+        if display_read.poll(0.001):
+            display_read.recv()
             sent = False
         pre = now
 
@@ -69,7 +82,6 @@ def reset(enigma, enigma_read):
         if pre and not now and not sent:
             sent = True
             enigma.send(True)
-            print("reset - request")
         if enigma_read.poll(0.001):
             enigma_read.recv()
             sent = False
@@ -77,30 +89,36 @@ def reset(enigma, enigma_read):
 
 def enigma(display, led, reset, reset_read):
     while True:
+        # Reset
         if reset.poll(0.001):
             reset_read.send(True)
             reset.recv()
-            print("reset")
         led.send("ffff")
-        display.send("    ")
+        display.send("|")
 
+        # Set the first router
         led.send("bfff")
         letter = get_letter(reset)
         if not letter:
             continue
         first_rotor_position = ord(letter) - ord('A')
+        print(f"The first router: {first_rotor_position}")
 
+        # Set the second router
         led.send("nbff")
         letter = get_letter(reset)
         if not letter:
             continue
-        second_rotor_position =  ord(letter) - ord('A')
+        second_rotor_position = ord(letter) - ord('A')
+        print(f"The second router: {second_rotor_position}")
 
+        # Set the third router
         led.send("nnbf")
         letter = get_letter(reset)
         if not letter:
             continue
         third_rotor_position = ord(letter) - ord('A')
+        print(f"The third router: {third_rotor_position}")
 
         led.send("nnnn")
         machine = EnigmaMachine(first_rotor_position, second_rotor_position, third_rotor_position)
@@ -109,7 +127,9 @@ def enigma(display, led, reset, reset_read):
             letter = get_letter(reset)
             if not letter:
                 break
-            display.send(machine.encrypt(letter))
+            encrypted = machine.encrypt(letter)
+            print(f"{letter} => {encrypted}")
+            display.send(encrypted)
 
 def get_letter(reset):
     while not reset.poll(0.001):
@@ -118,13 +138,14 @@ def get_letter(reset):
             key = evdev.ecodes.KEY[event.code]  # Get key name
             if event.value == 1 and key.startswith("KEY_") and len(key)==5:
                 alphanumeric = key[-1]
-                print(alphanumeric)
-                if "A"<=alphanumeric<="Z":
+                if "A" <= alphanumeric <= "Z":
                     return alphanumeric
 
 if __name__ == "__main__":
     display_pipe, enigma_pipe_display = Pipe()
-    display_process = Process(target=display, args=(display_pipe,))
+    replay_pipe, display_pipe_replay = Pipe()
+    replay_read_pipe, display_pipe_replay_read = Pipe()
+    display_process = Process(target=display, args=(display_pipe, display_pipe_replay, display_pipe_replay_read))
     display_process.start()
 
     led_pipe, enigma_pipe_led = Pipe()
@@ -138,6 +159,9 @@ if __name__ == "__main__":
 
     reset_process = Process(target=reset, args=(reset_pipe, reset_read_pipe))
     reset_process.start()
+
+    replay_process = Process(target=replay, args=(replay_pipe, replay_read_pipe))
+    replay_process.start()
 
     enigma_process.join()
     display_process.terminate()
